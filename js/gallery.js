b2088e2b739b40b23d30b6d7f5e705c94f00b68c
MaximusGreen <154276386+MaximusGreen@users.noreply.github.com>
2024-05-03 19:12:05 +0300

    1

--- js/gallery.js
+++ js/gallery.js
@@ -1,3 +1,5 @@
+
+import * as basicLightbox from 'basiclightbox';
 const images = [
   {
     preview:
@@ -65,3 +67,39 @@ const images = [
 ];
 
 
+
+const galleryContainer = document.querySelector('.gallery');
+
+const galleryItems = images.map(image => {
+  return `
+    <li class="gallery-item">
+      <a class="gallery-link" href="${image.original}">
+        <img
+          class="gallery-image"
+          src="${image.preview}"
+          data-source="${image.original}"
+          alt="${image.description}"
+        />
+      </a>
+    </li>
+  `;
+});
+
+galleryContainer.innerHTML = galleryItems.join('');
+
+const gallery = document.querySelector('.gallery');
+
+gallery.addEventListener('click', e => {
+  e.preventDefault();
+  
+  if (e.target.tagName !== 'IMG') return; 
+
+  const largeImageSrc = e.target.dataset.source;
+  const description = e.target.alt;
+
+  const instance = basicLightbox.create(`
+    <img src="${largeImageSrc}" alt="${description}" width="800" height="600">
+  `);
+
+  instance.show();
+});
